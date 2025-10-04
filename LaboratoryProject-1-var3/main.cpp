774428728f35dc5e47dcc09ed9b257ea70247fa0
Miclasher <14122006mg@gmail.com>
2025-10-04 23:10:03 +0300

    Implement main()

--- LaboratoryProject-1-var3/main.cpp
+++ LaboratoryProject-1-var3/main.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
-
 //Compiler: MSVC
 //Developers: Mykhailo Geilovych & Vladyslav Barylo
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <iostream>
+#include <stdexcept>
+
+#include "DataTypes.h"
+#include "FileUtil.h"
+#include "FrequencyTester.h"
+#include "ReportPrinter.h"
+
+int main(int argc, char* argv[]) {
+    try {
+        if (argc != 2) {
+            std::cerr << "Error: Must provide file path argument.\n";
+            std::cerr << "Usage example: " << argv[0] << " input.txt\n";
+            return 1;
+        }
+
+        std::cout << "Reading from file '" << argv[1] << "'...\n";
+        SimulationData data = FileUtils::readData(argv[1]);
+
+        std::cout << "Starting simulation...\n";
+        FrequencyTester tester(data);
+        tester.run();
+        std::cout << "Simulation finished.\n\n";
+
+        ReportPrinter::print(data, tester.getResults());
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "\nCritical error: " << e.what() << std::endl;
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "\nCritical error: Unknown error" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
